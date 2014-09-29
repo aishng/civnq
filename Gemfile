2b251c8ed888526d76782aceb270691629486d7c
Tanoy Sinha <tsinha@gmail.com>
2014-09-29 11:38:58 -0400

    initial commit

---
+++ Gemfile
@@ -0,0 +1,18 @@
+source 'https://rubygems.org'
+
+gem 'rails', '4.0.3'
+gem 'mysql2'
+gem 'sass-rails', '~> 4.0.0'
+gem 'uglifier', '>= 1.3.0'
+gem 'jquery-rails'
+
+gem 'turbolinks'
+
+group :doc do
+  # bundle exec rake doc:rails generates the API under doc/api.
+  gem 'sdoc', require: false
+end
+
+group :development do
+  gem 'byebug'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Civnq::Application.config.secret_key_base = '6695fe6bb38c7eb67291b7d048d70db761116404a705ad44a7c9475c6fb1f3cb56814447213bbf57a02f2d87e9b8855ad122617a0bcf57203c950d6d362486f4'
